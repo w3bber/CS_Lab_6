5630b8cd2393a4d954d48b8cffe92faae3693a0e
Класс <class@vyatsu.ru>
2023-11-03 11:23:00 +0300

    03.11.2023

---
+++ 6.2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 15;
+            int[] arr_1 = new int[SIZE] { 10, 9, 9, 8, 7, 6, 4, 3, 3, 2, 2, 2, 1, 1, 0 };
+            int[] arr_2 = new int[SIZE] { 10, 10, 9, 9, 7, 6, 6, 4, 3, 3, 3, 2, 2, 0, 0 };
+            Array.Reverse(arr_1);
+            Array.Reverse(arr_2);
+            int[] result_arr = FindIntersection(arr_1, arr_2);
+            print(result_arr);
+        }
+        static int[] FindIntersection(int[] arr_1, int[] arr_2)
+        {
+            int[] result_arr = new int[arr_1.Length + arr_2.Length];
+            int intersectionIndex = 0;
+
+            int index_1 = 0;
+            int index_2 = 0;
+
+            while(index_1 < arr_1.Length && index_2 < arr_2.Length)
+            {
+                if (arr_1[index_1] < arr_2[index_2])
+                {
+                    index_1++;
+                }
+                else if (arr_1[index_1] > arr_2[index_2])
+                {
+                    index_2++;
+                }
+                else
+                {
+                    result_arr[intersectionIndex] = arr_1[index_1];
+                    intersectionIndex++;
+
+                    index_1++;
+                    index_2++;
+                }
+            }
+
+            Array.Resize(ref result_arr, intersectionIndex);
+            return result_arr;
+        }
+
+        static void print(int[] array)
+        {
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+        }
+        static int[] arrayInput(int SIZE)
+        {
+            int[] array = new int[SIZE];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите {i + 1}-й элемент массива: ");
+                if (!int.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+                    i--; // Повторяем запрос для данного элемента
+                }
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ 6.3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            task();
+
+            
+        }
+
+        static void task()
+        {
+            try
+            {
+                Console.Write("Введите размерность массива: ");
+                int SIZE = int.Parse(Console.ReadLine());
+                int[] arr = new int[SIZE];
+                Random r = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = r.Next(-15, 31);
+                }
+                Console.WriteLine("Исходный массив: ");
+                print(arr);
+                int minValue = arr.Min();
+                int maxValue = arr.Max();
+                Console.WriteLine("Максимальный элемент: " + maxValue);
+                Console.WriteLine("Минимальный элемент: " + minValue);
+                int[] newArr = new int[arr.Length + 1];
+                int newIndex = 0;
+                bool inserted = false;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == maxValue && !inserted)
+                    {
+                        newArr[newIndex] = minValue;
+                        newIndex++;
+                        inserted = true;
+                    }
+                    newArr[newIndex] = arr[i];
+                    newIndex++;
+                }
+                Console.WriteLine("Изменённый массив");
+                print(newArr);
+                
+                Console.ReadKey();
+            }
+            catch(Exception ex) 
+            { 
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+            
+
+        }
+        static void print(int[] array, int count = -1)
+        {
+            if(count == -1)
+            {
+                count = array.Length;
+            }
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+        static int[] arrayInput(int SIZE)
+        {
+            int[] array = new int[SIZE];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите {i + 1}-й элемент массива: ");
+                if (!int.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+                    i--; // Повторяем запрос для данного элемента
+                }
+            }
+            return array;
+        }
+    }
+}
